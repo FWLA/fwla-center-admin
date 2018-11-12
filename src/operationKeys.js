b359e5d1a17f13a51b89d3fe526ed83087da2843
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2018-11-12 11:54:42 +0100

    Add operation key information

--- src/operationKeys.js
+++ src/operationKeys.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List, Datagrid, TextField, SelectField, EditButton, DeleteButton, Create, SimpleForm, TextInput, SelectInput, Edit, DisabledInput } from 'react-admin';
+import { List, Datagrid, TextField, SelectField, BooleanField, EditButton, DeleteButton, Create, SimpleForm, TextInput, SelectInput, BooleanInput, Edit, DisabledInput } from 'react-admin';
 
 const operationTypes = [
     { id: 'FIRE', name: 'Fire' },
@@ -10,7 +10,9 @@ export const OperationKeysList = (props) => (
     <List {...props}>
         <Datagrid>
             <TextField source="key" label="Key" />
+            <TextField source="code" label="Code" />
             <SelectField source="type" label="Type" choices={operationTypes} />
+            <BooleanField source="dangerToLife" label="Danger to life" />
             <EditButton />
             <DeleteButton />
         </Datagrid>
@@ -21,7 +23,9 @@ export const OperationKeysCreate = (props) => (
     <Create {...props}>
         <SimpleForm>
             <TextInput source="key" label="Key" />
+            <TextInput source="code" label="Code" />
             <SelectInput source="type" label="Type" choices={operationTypes} />
+            <BooleanInput source="dangerToLife" label="Danger to life" />
         </SimpleForm>
     </Create>
 );
@@ -31,7 +35,9 @@ export const OperationKeysEdit = (props) => (
         <SimpleForm>
             <DisabledInput source="id" />
             <TextInput source="key" label="Key" />
+            <TextInput source="code" label="Code" />
             <SelectInput source="type" label="Type" choices={operationTypes} />
+            <BooleanInput source="dangerToLife" label="Danger to life" />
         </SimpleForm>
     </Edit>
 );
