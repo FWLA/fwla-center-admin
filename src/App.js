5df3d45d8bee7568452614b52cfc436480617df2
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2018-12-27 00:28:05 +0100

    Add ResourceKeyPattern
    Closes #4.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import { OperationKeysCreate, OperationKeysEdit, OperationKeysList } from './ope
 import { RealEstateCreate, RealEstateEdit, RealEstateList } from './realEstates';
 import { RegexPatternsEdit, RegexPatternsList } from './regexPatterns';
 import { StationCreate, StationEdit, StationList } from './stations';
+import { ResourceKeyPatternsList, ResourceKeyPatternsCreate, ResourceKeyPatternsEdit } from './resourceKeyPatterns';
 
 
 const messages = {
@@ -26,6 +27,7 @@ const App = () => (
     <Resource name="regexPatterns" options={{ label: 'Regex Patterns' }} list={RegexPatternsList} edit={RegexPatternsEdit} icon={CodeIcon} />
     <Resource name="stations" options={{ label: 'Standorte' }} list={StationList} create={StationCreate} edit={StationEdit} icon={HomeIcon} />
     <Resource name="operationKeys" options={{ label: 'Einsatzstichworte' }} list={OperationKeysList} create={OperationKeysCreate} edit={OperationKeysEdit} icon={WhatshotIcon} />
+    <Resource name="resourceKeyPatterns" options={{ label: 'Einsatzmittel-Pattern' }} create={ResourceKeyPatternsCreate} edit={ResourceKeyPatternsEdit} list={ResourceKeyPatternsList} icon={CodeIcon} />
     <Resource name="resources" options={{ label: 'Fahrzeuge' }} list={ResourceList} create={ResourceCreate} edit={ResourceEdit} icon={DriveEtaIcon} />
     <Resource name="realEstates" options={{ label: 'Objekte' }} list={RealEstateList} create={RealEstateCreate} edit={RealEstateEdit} icon={HomeIcon} />
     <Resource name="eventLogs" options={{ label: 'Logs' }} list={EventLogList} icon={InfoIcon} />
---
+++ src/resourceKeyPatterns.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { List, Datagrid, TextField, SelectField, EditButton, SimpleForm, TextInput, SelectInput, Create, Edit, DisabledInput } from 'react-admin';
+
+const modes = [
+    { id: 'INCLUDE', name: 'einschließen'},
+    { id: 'EXCLUDE', name: 'ausschließen' },
+];
+
+export const ResourceKeyPatternsList = (props) => (
+    <List title="Fahrzeug-Schlüssel Patterns" {...props}>
+        <Datagrid>
+            <TextField source="pattern" label="Pattern" />
+            <SelectField source="mode" label="Modus" choices={modes} />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+export const ResourceKeyPatternsCreate = (props) => (
+    <Create title="Fahrzeug-Schlüssel Pattern erstellen" {...props}>
+        <SimpleForm>
+            <TextInput source="pattern" label="Pattern" />
+            <SelectInput source="mode" label="Modus" choices={modes} />
+        </SimpleForm>
+    </Create>
+);
+
+export const ResourceKeyPatternsEdit = (props) => (
+    <Edit title="Fahrzeug-Schlüssel Pattern bearbeiten" {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" label="ID" />
+            <TextInput source="pattern" label="Pattern" />
+            <SelectInput source="mode" label="Modus" choices={modes} />
+        </SimpleForm>
+    </Edit>
+);
