151b88281d2e75996bb12bb4d4b91030ff0d18d8
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2019-08-19 21:05:09 +0200

    Add category to layer groups

--- src/MapLayer.js
+++ src/MapLayer.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { Create, Datagrid, DeleteButton, DisabledInput, Edit, EditButton, List, maxLength, required, SimpleForm, TextField, TextInput } from 'react-admin';
+import { Create, Datagrid, DeleteButton, DisabledInput, Edit, EditButton, List, maxLength, required, SelectField, SelectInput, SimpleForm, TextField, TextInput } from 'react-admin';
 import { JsonField } from './fields/JsonField';
 
+const categories = [
+    { id: 'OPERATION', name: 'Einsatz' },
+    { id: 'INFO', name: 'Info' },
+    { id: 'ARCHIVE', name: 'Archiv' },
+];
+
 const validateName = [required(), maxLength(64)];
+const validateCategory = [required(), maxLength(64)];
 
 export const MapLayerList = (props) => (
     <List title="Kartenebenen" sort={{ field: 'name', order: 'ASC' }} {...props}>
         <Datagrid>
             <TextField source="name" label="Bezeichnung" />
+            <SelectField source="category" label="Kategorie" choices={categories} />
             <JsonField source="geoJson" label="Geo JSON" />
             <EditButton />
             <DeleteButton />
@@ -19,6 +27,7 @@ export const MapLayerCreate = (props) => (
     <Create title="Kartenebene erstellen" {...props}>
         <SimpleForm>
             <TextInput source="name" label="Bezeichnung" validate={validateName} />
+            <SelectInput source="category" label="Kategorie" choices={categories} validate={validateCategory} />
             <JsonField source="geoJson" label="GeoJSON" />
         </SimpleForm>
     </Create>
@@ -29,6 +38,7 @@ export const MapLayerEdit = (props) => (
         <SimpleForm>
             <DisabledInput source="id" label="ID" />
             <TextInput source="name" label="Bezeichnung" validate={validateName} />
+            <SelectInput source="category" label="Kategorie" choices={categories} validate={validateCategory} />
             <JsonField source="geoJson" label="GeoJSON" />
         </SimpleForm>
     </Edit>
