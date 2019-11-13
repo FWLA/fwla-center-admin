ae265a505cd24fb58b45a7d17abfcfb2c100891e
Benjamin Ihrig <benjamin.ihrig@gmail.com>
2019-11-13 13:54:29 +0100

    Add station to display event.

--- src/DisplayEvent.js
+++ src/DisplayEvent.js
@@ -1,6 +1,6 @@
 import RichTextInput from 'ra-input-rich-text';
 import React from 'react';
-import { BooleanField, BooleanInput, Create, Datagrid, DateField, DeleteButton, DisabledInput, Edit, EditButton, List, SimpleForm } from 'react-admin';
+import { BooleanField, BooleanInput, Create, Datagrid, DateField, DeleteButton, DisabledInput, Edit, EditButton, List, ReferenceField, ReferenceInput, SelectInput, SimpleForm, TextField } from 'react-admin';
 import { DateTimeInput } from 'react-admin-date-inputs';
 
 const validateDisplayEvent = (value) => {
@@ -18,6 +18,7 @@ const validateDisplayEvent = (value) => {
     return errors;
 };
 
+const validateStation = [];
 const validateStartTime = [];
 const validateEndTime = [];
 const validateText = [];
@@ -26,6 +27,9 @@ const validateShowOperation = [];
 export const DisplayEventList = (props) => (
     <List title="Anzeige" sort={{ field: 'startTime', order: 'ASC' }} {...props}>
         <Datagrid>
+            <ReferenceField source="stationId" reference="stations" label="Standort">
+                <TextField source="name" />
+            </ReferenceField>
             <DateField source="startTime" label="Start-Zeit" showTime />
             <DateField source="endTime" label="End-Zeit" showTime />
             <BooleanField source="showOperation" label="Einsatz anzeigen" />
@@ -38,6 +42,9 @@ export const DisplayEventList = (props) => (
 export const DisplayEventCreate = (props) => (
     <Create title="Anzeige erstellen" {...props}>
         <SimpleForm validate={validateDisplayEvent}>
+            <ReferenceInput label="Standort" source="stationId" reference="stations" validate={validateStation}>
+                <SelectInput optionText="name" optionValue="id" />
+            </ReferenceInput>
             <DateTimeInput source="startTime" label="Start-Zeit" validate={validateStartTime} options={{ format: 'dd.MM.yyyy, HH:mm:ss', ampm: false, clearable: true }} />
             <DateTimeInput source="endTime" label="End-Zeit" validate={validateEndTime} options={{ format: 'dd.MM.yyyy, HH:mm:ss', ampm: false, clearable: true }} />
             <RichTextInput source="text" label="Text" validate={validateText} />
@@ -50,6 +57,9 @@ export const DisplayEventEdit = (props) => (
     <Edit title="Anzeige bearbeiten" {...props}>
         <SimpleForm validate={validateDisplayEvent}>
             <DisabledInput source="id" label="ID" />
+            <ReferenceInput label="Standort" source="stationId" reference="stations" validate={validateStation}>
+                <SelectInput optionText="name" optionValue="id" />
+            </ReferenceInput>
             <DateTimeInput source="startTime" label="Start-Zeit" validate={validateStartTime} options={{ format: 'dd.MM.yyyy, HH:mm:ss', ampm: false, clearable: true }} />
             <DateTimeInput source="endTime" label="End-Zeit" validate={validateEndTime} options={{ format: 'dd.MM.yyyy, HH:mm:ss', ampm: false, clearable: true }} />
             <RichTextInput source="text" label="Text" validate={validateText} />
